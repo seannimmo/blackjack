b8da8dd3bf6dfc0220b32886ce0f8d73d07ee2d1
seannimmo <Sean@Seans-MacBook-Pro.local>
2019-05-24 11:14:14 +0200

    card mount and render

--- src/js/card.js
+++ src/js/card.js
@@ -7,4 +7,18 @@ class Card {
     this.suit = suit;
     this.value = value;
   }
+
+  mount() {
+    const hand = document.querySelector("#player-hand > .cards");
+    hand.appendChild(this.render());
+
+  }
+
+  render () {
+    this.element = createElement('div');
+    this.element.className = `card face-${rank}-of-${suit}`;
+    console.log(this.element.className);
+    return this.element; 
+  }
+
 } 
